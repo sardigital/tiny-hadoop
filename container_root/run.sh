5b0a780cba72abd537a7fd54fc9f92a1b6b2585b
konstantin golovko <konstantin.golovko@exactprosystems.com>
2021-09-15 11:42:28 +0400

    up versions

--- container_root/run.sh
+++ container_root/run.sh
@@ -50,8 +50,8 @@ echo "Done"
 /opt/hadoop/bin/hdfs dfs -ls /user/oozie/share/lib
 if [ $? -eq 1 ]; then
     echo "Preparing Oozie"
-    su -l -c "cd /opt/oozie && bin/ooziedb.sh create -sqlfile oozie.sql -run"
-    su -l -c "bin/oozie-setup.sh"
+    su -l -c "cd /opt/oozie && bin/ooziedb.sh create -sqlfile data/oozie-db/oozie.sql -run"
+    # su -l -c "bin/oozie-setup.sh"
     su -l -c "bin/oozie-setup.sh sharelib create -fs hdfs://localhost:8020  -locallib oozie-sharelib-4.3.1.tar.gz" oozie
 fi
 
